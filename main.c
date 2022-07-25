e2d39445813ae1723a0dd9348a84872da6d4705d
a6888b <ayoubbrahim68@gmail.com>
2022-07-25 22:28:38 +0200

    ajout condition est demande d'entrez

--- main.c
+++ main.c
@@ -9,11 +9,29 @@ int number_random(int max);
 
 int main(void)
 {
-    bool stop = false; 
-    
-    while (! stop){
-        int nb_rand = number_random(MAX_RAND);
-        
+    bool stop = false;
+    int nb_rand = number_random(MAX_RAND);
+    int number_enter;
+
+    while (!stop)
+    {
+        printf("Entrez un nombre: ");
+        scanf("%d", &number_enter);
+
+        if (number_enter == number_random)
+        {
+            printf("Vous avez trouver le bon nombre !");
+            stop = true;
+        }
+
+        else if (number_enter > nb_rand)
+        {
+            printf("Le nombre entrez est plus grand que le nombre mystere !");
+        }
+        else
+        {
+            printf("Le nombre entrez est plus petit que le nombre mystere !");
+        }
     }
 
     return 0;
@@ -22,5 +40,5 @@ int main(void)
 int number_random(int max)
 {
     srand(time(NULL));
-    return (rand() % max) ;
+    return (rand() % max);
 }
