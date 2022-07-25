f1074c6734804b8f95cd51df6ddfa7435b5904d2
a6888b <ayoubbrahim68@gmail.com>
2022-07-25 22:04:41 +0200

    ajout boucle

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 
 int number_random(int max);
 
 int main(void)
 {
+    bool stop = false; 
+    
+    while (! stop){
+        
+    }
+
     return 0;
 }
 
